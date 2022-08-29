941c618f6b2d7d92d67118234bb3c5267496bfea
Sarkhan <sarkhanjafarli12@gmail.com>
2022-08-29 21:25:35 +0400

    Initial Commit

---
+++ PlayingCardsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCardsApp
+//
+//  Created by Sarkhan Jafarli on 8/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
